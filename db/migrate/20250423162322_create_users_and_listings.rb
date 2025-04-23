3a9aa2192433ae11f9827f9bc1c729d5cf683c0d
Justin Searls <searls@gmail.com>
2025-04-23 12:49:26 -0400

    WIP

---
+++ db/migrate/20250423162322_create_users_and_listings.rb
@@ -0,0 +1,48 @@
+class CreateUsersAndListings < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.timestamps
+
+      t.index :email, unique: true
+    end
+
+    create_table :listings do |t|
+      t.string :source, null: false
+      t.string :suumo_id, null: false
+      t.string :suumo_url, null: false
+
+      t.string :title
+      t.string :url, null: false
+      t.string :price_description
+      t.integer :price_man_en
+      t.string :location_description
+      t.string :prefecture
+      t.string :city
+      t.string :ward
+      t.string :town
+      t.string :chome
+      t.string :block_number
+      t.string :building_number
+      t.string :building_name
+      t.string :postal_code
+
+      t.string :station_description
+      t.integer :station_minutes_walk
+      t.integer :station_meters_distance
+      t.string :area_description # 71.71m2～85.22m2
+      t.integer :area_square_centimeters # 784600
+      t.string :layout_description # 2LDK・3LDK
+      t.string :balcony_description
+      t.string :construction_date_description # 2025年6月下旬予定
+      t.integer :construction_year
+      t.boolean :construction_complete
+      t.boolean :used
+
+      t.datetime :refreshed_at, null: false
+      t.timestamps
+
+      t.index :suumo_id, unique: true, where: "suumo_id IS NOT NULL"
+    end
+  end
+end
